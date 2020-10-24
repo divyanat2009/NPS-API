4a3f20f191d2b0ae6ef79f9e3a0dd972a1038048
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-24 17:14:34 -0500

    added addresses.type

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function renderParkResults(parkList){
     $('#results').text("Check your search result below:");
     parkList.forEach(item =>{
     $('#results-list').append(`<li><h3>${item.fullName}</h3>
-    <p>${item.description}</p><a href=${item.url}>Park's Website</a><h5>${item.addresses}</h5>
+    <p>${item.description}</p><a href=${item.url}>Park's Website</a><h5>${item.addresses.type}:</h5><p>${item.addresses.line1}</p><p>${item.addresses.line2}</p><p>${item.addresses.line3}</p>
     <p>${item.addresses.city}, ${item.addresses.statecode}, ${item.addresses.postalcode}</p></li>`)
     });
 }
