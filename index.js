3ce72bb36eddc59be95c2fd1ad79344c85ea8353
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-24 17:46:58 -0500

    added class to p tag

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function renderParkResults(parkList){
     $('#results').text("Check your search result below:");
     parkList.forEach(item =>{
     $('#results-list').append(`<li><h3>${item.fullName}</h3>
-    <p>${item.description}</p><a href=${item.url}>Park's Website</a><h5>${item.addresses.type}:</h5><p>${item.addresses.line1}</p><p>${item.addresses.line2}</p><p>${item.addresses.line3}</p>
-    <p>${item.addresses.city}, ${item.addresses.stateCode}, ${item.addresses.postalCode}</p></li>`)
+    <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses.type}:</h5><p class="addresses">${item.addresses.line1}</p><p class="addresses">${item.addresses.line2}</p><p class="addresses">${item.addresses.line3}</p>
+    <p class="addresses">${item.addresses.city}, ${item.addresses.stateCode}, ${item.addresses.postalCode}</p></div></li>`)
     });
 }
 
