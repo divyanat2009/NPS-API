c062d81c0c7be087a31be886fa2adff1b051a881
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-24 21:48:09 -0500

    added index to address

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function getParkResults(query, maxResults=10){
       maxResults
   };
   const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString + '&fields=addresses'
+  const url = searchURL + '?' + queryString
     
   fetch(url)
     .then(response =>{
@@ -39,8 +39,8 @@ function renderParkResults(parkList){
     $('#results').text("Check your search result below:");
     parkList.forEach(item =>{
     $('#results-list').append(`<li><h3>${item.fullName}</h3>
-    <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses.type}:</h5><p class="addresses">${item.addresses.line1}</p><p class="addresses">${item.addresses.line2}</p><p class="addresses">${item.addresses.line3}</p>
-    <p class="addresses">${item.addresses.city}, ${item.addresses.stateCode}, ${item.addresses.postalCode}</p></div></li>`)
+    <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses[0].type}:</h5><p class="addresses">${item.addresses[0].line1}</p><p class="addresses">${item.addresses[0].line2}</p><p class="addresses">${item.addresses[0].line3}</p>
+    <p class="addresses">${item.addresses[0].city}, ${item.addresses[0].stateCode}, ${item.addresses[0].postalCode}</p></div></li>`)
     });
 }
 
